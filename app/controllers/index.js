ff84ae0f0601972ae6b5c22ffd2c21ac30c26b65
David Mörtsell <fiskjag@gmail.com>
2016-02-12 23:52:32 +0100

    Header/breadcrumb fixes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	// actions: {
-	// 	createLeague: function() {
-	// 		var newLeague = this.store.createRecord('league', {
-	// 			year: this.get('year')
-	// 		});
-	// 		newLeague.save();
-	// 	}
-	// }
 });
