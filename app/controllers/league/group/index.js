22ce98fa7e6458f2f9737fa8ac02b91d7ca8a59c
David Mörtsell <fiskjag@gmail.com>
2016-02-15 16:13:54 +0100

    Update dates and order matches on change

--- app/controllers/league/group/index.js
+++ app/controllers/league/group/index.js
@@ -5,5 +5,18 @@ export default Ember.Controller.extend({
 	sortedTeams: Ember.computed.sort('model.teams', 'teamSortProps'),
 
 	matchSortProps: ['date'], 
-	sortedMatches: Ember.computed.sort('model.matches', 'matchSortProps')
+	sortedMatches: Ember.computed.sort('model.matches', 'matchSortProps'),
+
+	actions: {
+	    update(updatedMatch) {
+	      	var group = this.model.group;
+	      
+		    this.store.findRecord('match', updatedMatch.id).then(function(match) {
+			    match.set('date', new Date(updatedMatch.value));
+			    match.save().then(function () {
+			    	group.save();
+			    });
+		    });
+	    }
+  	}
 });
