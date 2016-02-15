20d92735511d01612c111a5715ed65e0bb2e8a90
David Mörtsell <fiskjag@gmail.com>
2016-02-15 22:42:56 +0100

    Started handling result update

--- app/controllers/league/group/index.js
+++ app/controllers/league/group/index.js
@@ -47,8 +47,50 @@ export default Ember.Controller.extend({
 			    	group.save();
 			    });
 		    });
+	    },
+	    updateResults() {
+	    	var group = this.model.group;
+        	var teams = group.get('teams');
+	        var matches = group.get('matches');
+
+	        for(var j = 0; j < matches.length; j++) {
+	        	var hometeam = teams.findBy('name', matches.objectAt(j).get('hometeam'));
+	        	var awayteam = teams.findBy('name', matches.objectAt(j).get('awayteam'));
+
+	            var hometeamwin = (matches.objectAt(j).get('homegoals') > matches.objectAt(j).get('awaygoals') ? true : false);
+	            var draw = (matches.objectAt(j).get('homegoals') === matches.objectAt(j).get('awaygoals') ? true : false);
+	            var awayteamwin = (matches.objectAt(j).get('homegoals') < matches.objectAt(j).get('awaygoals') ? true : false);
+
+	            if((matches.objectAt(j).get('homegoals') !== null && matches.objectAt(j).get('homegoals') !== '' && !isNaN(matches.objectAt(j).get('homegoals'))) ||
+	               (matches.objectAt(j).get('awaygoals') !== null && matches.objectAt(j).get('awaygoals') !== '' && !isNaN(matches.objectAt(j).get('awaygoals')))) {
+	                var hometeamgoalsscored = (matches.objectAt(j).get('homegoals') !== null && matches.objectAt(j).get('homegoals') !== '' && !isNaN(matches.objectAt(j).get('homegoals'))) ? parseInt(matches.objectAt(j).get('homegoals')) : 0;
+	                var hometeamgoalsagainst = (matches.objectAt(j).get('awaygoals') !== null && matches.objectAt(j).get('awaygoals') !== '' && !isNaN(matches.objectAt(j).get('awaygoals'))) ? parseInt(matches.objectAt(j).get('awaygoals')) : 0;
+	                var awayteamgoalsscored = (matches.objectAt(j).get('awaygoals') !== null && matches.objectAt(j).get('awaygoals') !== '' && !isNaN(matches.objectAt(j).get('awaygoals'))) ? parseInt(matches.objectAt(j).get('awaygoals')) : 0;
+	                var awayteamgoalsagainst = (matches.objectAt(j).get('homegoals') !== null && matches.objectAt(j).get('homegoals') !== '' && !isNaN(matches.objectAt(j).get('homegoals'))) ? parseInt(matches.objectAt(j).get('homegoals')) : 0;
+
+	                hometeam.incrementProperty('gamesplayed');
+	                hometeam.incrementProperty('wins', (hometeamwin ? 1 : 0));
+	                hometeam.incrementProperty('ties', (draw ? 1 : 0));
+	                hometeam.incrementProperty('losses', (awayteamwin ? 1 : 0));
+	                hometeam.incrementProperty('goalsscored', hometeamgoalsscored);
+	                hometeam.incrementProperty('goalsagainst', hometeamgoalsagainst);
+	                hometeam.incrementProperty('points', (hometeamwin ? 3 : (draw ? 1 : 0)));
+
+	                awayteam.incrementProperty('gamesplayed');
+	                awayteam.incrementProperty('wins', (awayteamwin ? 1 : 0));
+	                awayteam.incrementProperty('ties', (draw ? 1 : 0));
+	                awayteam.incrementProperty('losses', (hometeamwin ? 1 : 0));
+	                awayteam.incrementProperty('goalsscored', awayteamgoalsscored);
+	                awayteam.incrementProperty('goalsagainst', awayteamgoalsagainst);
+	                awayteam.incrementProperty('points', (awayteamwin ? 3 : (draw ? 1 : 0)));
+
+	                hometeam.save().then(function() {
+	                	awayteam.save().then(function() {
+	                		group.save();
+	                	});
+	                });
+	            }
+	        }
 	    }
-
-
   	}
 });
